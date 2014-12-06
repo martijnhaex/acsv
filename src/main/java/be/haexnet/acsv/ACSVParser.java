1fc01d183dcf3ff871d2038cfc2206c960bc16e9
martijnhaex <martijn.haex@c4j.be>
2014-12-06 15:54:28 +0100

    Fix Build.

--- src/main/java/be/haexnet/acsv/ACSVParser.java
+++ src/main/java/be/haexnet/acsv/ACSVParser.java
@@ -3,6 +3,7 @@ package be.haexnet.acsv;
 import au.com.bytecode.opencsv.CSV;
 import au.com.bytecode.opencsv.CSVReadProc;
 import be.haexnet.acsv.annotation.ACSVColumn;
+import be.haexnet.acsv.exception.ACSVConfigurationException;
 import be.haexnet.acsv.util.AnnotationUtil;
 import be.haexnet.acsv.util.ReflectionUtil;
 
@@ -18,11 +19,14 @@ public class ACSVParser<Entity> {
         final String[] rawHeader = extractRawHeader(rows);
         final List<String[]> rawData = extractRawData(rows);
 
+        final List<Field> annotatedFields = AnnotationUtil.getAnnotatedFieldsOf(targetClass, ACSVColumn.class);
+        validateHeader(rawHeader, annotatedFields);
+
         final List<Entity> parsedResult = new ArrayList<>();
         for (final String[] rawDataLine : rawData) {
             final Entity newInstance = (Entity) ReflectionUtil.createNewInstanceFor(targetClass);
 
-            for (final Field declaredField : AnnotationUtil.getAnnotatedFieldsOf(newInstance, ACSVColumn.class)) {
+            for (final Field declaredField : annotatedFields) {
                 if (declaredField.isAnnotationPresent(ACSVColumn.class)) {
                     final String declaredFieldName = declaredField.getName();
 
@@ -37,6 +41,21 @@ public class ACSVParser<Entity> {
             parsedResult.add(newInstance);
         }
         return parsedResult;
+    }
+
+    private void validateHeader(final String[] rawHeader, final List<Field> annotatedFields) {
+        if (rawHeader.length == annotatedFields.size()) {
+            for (int i = 0; i < rawHeader.length; i++) {
+                final String rawHeaderName = rawHeader[i];
+                final Field annotatedField = annotatedFields.get(i);
+
+                if (!rawHeaderName.equals(annotatedField.getName())) {
+                    throw new ACSVConfigurationException("Not expecting header found: " + rawHeaderName + ".");
+                }
+            }
+        } else {
+            throw new ACSVConfigurationException("Headers in CSV file not equal to annotated target fields.");
+        }
     }
 
     private List<String[]> getAllRowsFromFile(final File file) {
