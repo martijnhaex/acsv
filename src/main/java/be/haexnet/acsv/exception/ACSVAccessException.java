f78c274b2667ab31670d70bf1e4f4771866bfe3e
martijnhaex <martijn.haex@c4j.be>
2014-12-06 15:06:39 +0100

    Introduce custom exceptions for misconfiguration and access.

---
+++ src/main/java/be/haexnet/acsv/exception/ACSVAccessException.java
@@ -0,0 +1,9 @@
+package be.haexnet.acsv.exception;
+
+public class ACSVAccessException extends RuntimeException {
+
+    public ACSVAccessException(final String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/be/haexnet/acsv/exception/ACSVConfigurationException.java
@@ -0,0 +1,9 @@
+package be.haexnet.acsv.exception;
+
+public class ACSVConfigurationException extends RuntimeException {
+
+    public ACSVConfigurationException(final String message) {
+        super(message);
+    }
+
+}
