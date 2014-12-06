e007758d7ee8c2fda0f1cb3b10a34bae3bc607b0
martijnhaex <martijn.haex@c4j.be>
2014-12-06 16:21:06 +0100

    Validate header and column count.

--- src/test/java/be/haexnet/acsv/ACSVParserIntegrationTest.java
+++ src/test/java/be/haexnet/acsv/ACSVParserIntegrationTest.java
@@ -1,7 +1,10 @@
 package be.haexnet.acsv;
 
 import be.haexnet.acsv.annotation.ACSVColumn;
+import be.haexnet.acsv.exception.ACSVConfigurationException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.io.File;
 import java.util.List;
@@ -9,7 +12,10 @@ import java.util.List;
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.fest.assertions.api.Assertions.extractProperty;
 
-public class ACSVParserTest {
+public class ACSVParserIntegrationTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     private ACSVParser<Person> parser = new ACSVParser();
 
@@ -20,6 +26,20 @@ public class ACSVParserTest {
         assertThat(extractProperty("firstName").from(parsedResult)).containsOnly("Martijn", "Timo");
         assertThat(extractProperty("lastName").from(parsedResult)).containsOnly("Haex", "Lemmens");
         assertThat(extractProperty("age").from(parsedResult)).containsOnly("24");
+    }
+
+    @Test
+    public void parseWillThrowErrorWhenColumnCountCSVIsNotEqualToAnnotatedFieldsCount() throws Exception {
+        expectedException.expect(ACSVConfigurationException.class);
+        expectedException.expectMessage("Headers in CSV file not equal to annotated target fields.");
+        parser.parse(file("person.csv"), PersonWithExtraField.class);
+    }
+
+    @Test
+    public void parseWillThrowErrorWhenColumnHeadersIsNotFoundAsAnnotatedField() throws Exception {
+        expectedException.expect(ACSVConfigurationException.class);
+        expectedException.expectMessage("Not expecting header found: age.");
+        parser.parse(file("person.csv"), PersonWithWrongFields.class);
     }
 
     private File file(final String fileName) {
@@ -46,6 +66,26 @@ public class ACSVParserTest {
         public String getFirstName() {
             return firstName;
         }
+    }
+
+    public static class PersonWithExtraField {
+        @ACSVColumn
+        public String firstName;
+        @ACSVColumn
+        public String lastName;
+        @ACSVColumn
+        public String middleName;
+        @ACSVColumn
+        public String age;
+    }
+
+    public static class PersonWithWrongFields {
+        @ACSVColumn
+        public String firstName;
+        @ACSVColumn
+        public String lastName;
+        @ACSVColumn
+        public String middleName;
     }
 
 }
