b87091ba300b303088fb8b9ecd1ff35d4fa65fa6
martijnhaex <martijn.haex@c4j.be>
2014-12-16 07:18:47 +0100

    Introduce NumberConverter. Introduce LongConverter.

--- src/main/java/be/haexnet/acsv/converter/IntegerConverter.java
+++ src/main/java/be/haexnet/acsv/converter/IntegerConverter.java
@@ -1,16 +1,10 @@
 package be.haexnet.acsv.converter;
 
-import be.haexnet.acsv.exception.ACSVFormatException;
-import org.apache.commons.lang3.math.NumberUtils;
-
-public class IntegerConverter implements TypeConverter<Integer> {
+public class IntegerConverter extends NumberConverter<Integer> {
 
     @Override
-    public Integer apply(final String value) {
-        if (NumberUtils.isNumber(value)) {
-            return Integer.valueOf(value);
-        }
-        throw new ACSVFormatException(value, Integer.class.getName());
+    Integer convert(final String value) {
+        return Integer.valueOf(value);
     }
 
 }
